6efccc1e0719efdaf2b83b60db5794c315a76b6d
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2021-12-30 11:13:46 +0000

    Add note: for loop, while loop and infinite loop

--- day-6/day-6-start.py
+++ day-6/day-6-start.py
@@ -5,7 +5,23 @@ print(num_char)
 
 #make own function
 def my_function(): #functions always has ()
-  print("Hello") #do this
-  print("Bye") #then do this
-
+    print("Hello") #do this
+    print("Bye") #then do this
+    #indentation - all functions under the indentation are in def
 my_function() #calling functions
+
+#for loop
+#for item in list_of_items:
+  #do something to each item
+#for number in range(1,10):
+  #print(number()
+
+#while loop
+  #while something_is_true
+    #do somethign repeatedly
+    #if something is wrong (false), the loop will stop
+
+#infinite loop
+  #do this
+  #then do thos
+  #then do this
