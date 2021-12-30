143e462010e3d07e92ea7b2fef8accab67b931cf
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2021-12-30 09:58:15 +0000

    Add day-6-start

---
+++ day-6/day-6-start.py
@@ -0,0 +1,11 @@
+#built-in function
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+#make own function
+def my_function(): #functions always has ()
+  print("Hello") #do this
+  print("Bye") #then do this
+
+my_function() #calling functions
