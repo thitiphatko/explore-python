10707106631f02d8b31bdfb90bb4189833bd9b85
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-04 09:41:04 +0000

    add day-8 folder and start

---
+++ day-8/day-8-start.py
@@ -0,0 +1,13 @@
+# Create a function
+def greet():
+    print("Hello")
+    print("How do you do?")
+    print("Isn't the weather nice today?")
+greet()
+
+# Function that allos for input
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+greet_with_name("Good")
+#Function is a parameter. Input is an argument.
