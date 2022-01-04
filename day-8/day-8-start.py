56d1be9e3dac17b3c09db75d87f110e3ef5d59b0
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-04 09:51:52 +0000

    add function with more inputs and types of argument

--- day-8/day-8-start.py
+++ day-8/day-8-start.py
@@ -9,5 +9,16 @@ greet()
 def greet_with_name(name):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
-greet_with_name("Good")
-#Function is a parameter. Input is an argument.
+greet_with_name("Joe")
+# Function is a parameter. Input is an argument.
+
+# Function with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}.")
+    print(f"What is it like in {location}.")
+greet_with(location = "UK", name = "Joe")
+
+# def my_function(a,b,c): positional arguments
+#     a = 1, b = 2, c = 3
+
+# def my_function( a = 1, b = 2, c = 3 ): keyword arguments
