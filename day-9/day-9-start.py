45f8937151947a22f1f3929033296a11acf6cf9f
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-20 17:44:15 +0000

    add day-9-start: nesting

--- day-9/day-9-start.py
+++ day-9/day-9-start.py
@@ -23,3 +23,37 @@ programming_dictionary["Bug"] = "A moth in your computer."
 for key in programming_dictionary:
     print (key)
     print(programming_dictionary[key])
+
+##########
+
+#Nesting 
+Capitals = {
+    "France", "Paris",
+    "Germany", "Berlin",
+}
+
+#Nesting a list in a dictionary
+travel_log = {
+    "France": {"cities_visited":["Paris", "Lille", "Dijon"]}, "total_visits": 12},
+    "Germany": {"cities_visited":["Berlin", "Hamburn", "Stuttgart"]}, "total_visits": 5},
+}
+
+#Nesting a disctionary in a dictionary
+travel_log = {
+    "France": {"cities_visited":["Paris", "Lille", "Dijon"]}, "total_visits": 12},
+    "Germany": {"cities_visited":["Berlin", "Hamburn", "Stuttgart"]}, "total_visits": 5},
+}
+
+#Nesting dictionary in a list
+travel_log = [
+    {
+        "country": "France", #string
+        "cities_visited":["Paris", "Lille", "Dijon"], #list
+        "total_visits": 12 #integer
+    },
+    {
+        "country": "Germany", 
+        "cities_visited":["Berlin", "Hamburn", "Stuttgart"], 
+        "total_visits": 5
+    },
+]
