d6feaa0676c18d18446e3d09aec031aabe6e3689
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-22 10:44:35 +0000

    Add day-10: function with output, change folders name

---
+++ day-10/day-10-start.py
@@ -0,0 +1,8 @@
+#Function with output
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}" #output
+
+print(format_name("thomas", "SHELBY"))
