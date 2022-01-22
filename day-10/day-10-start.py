3aa1395a3f87f43be8b8cbe090d9056a0b178b0e
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-22 10:51:17 +0000

    day-10-start: how to terminate function early if there is an invalid input

--- day-10/day-10-start.py
+++ day-10/day-10-start.py
@@ -1,8 +1,11 @@
 #Function with output
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs." #terminate function early
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}" #output
+    #if there are other lines of code after "return", it won't be executed as "return" is the output
 
-print(format_name("thomas", "SHELBY"))
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
