af33de51a1b08b5ba06ad0b6d0631652f7db46a4
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-22 12:09:16 +0000

    finish day-10 project: calculator

--- day-10/pj-calculator.py
+++ day-10/pj-calculator.py
@@ -1,5 +1,4 @@
 from art import logo
-print(logo)
 
 #add
 def add(n1, n2):
@@ -21,14 +20,27 @@ opertaions = {
     "/": divide
 }
 
-num1 = int(input("What is the first number?: "))
-for symbol in opertaions:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What is the second number?: "))
-calculation_function = opertaions[operation_symbol]
-answer = calculation_function(num1, num2)
+def calculator():
+    print(logo)
+
+    num1 = float(input("What is the first number?: "))
+    for symbol in opertaions:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        calculation_function = opertaions[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
 
 
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+calculator()
