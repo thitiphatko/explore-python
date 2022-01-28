981feb1583cacda6664bbc39af892fb62bd67eab
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-28 16:30:34 +0000

    update day-12: block scope

--- day-12/day-12-start.py
+++ day-12/day-12-start.py
@@ -1,31 +1,41 @@
 ################### Scope ####################
 
-enemies = 1
+# enemies = 1
 
-def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside function: {enemies}")
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
 
 # Local Scope
 #   new variables/functions created are accessible only within the function
 
-def drink_potion():
-    potion_str = 2
-    print(potion_str)
+# def drink_potion():
+#     potion_str = 2
+#     print(potion_str)
 
-drink_potion()
+# drink_potion()
 #print(potion_str)
 
 # Global Scope
-player_health = 10
+# player_health = 10
 
-def game():
-    def drink_potion():
-        potion_str = 2
-        print(player_health)
-    drink_potion()
+# def game():
+#     def drink_potion():
+#         potion_str = 2
+#         print(player_health)
+#     drink_potion()
 
-print(player_health)
+# print(player_health)
+
+# There is no Block Scope
+
+game_level = 3
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 5:
+        new_enemy = enemies[0]
+
+    print(new_enemy)
