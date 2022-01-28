08b9c26fd285a42e5ad0e3b52d1c91047533aefd
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-28 16:40:54 +0000

    update day-12: global constants

--- day-12/day-12-start.py
+++ day-12/day-12-start.py
@@ -42,14 +42,18 @@
 
 # Modifying Global Scope
 
-enemies = 1
+# enemies = 1
 
-def increase_enemies():
-    # global enemies
-    # to make variable accessible in a function
-    # enemies += 1
-    print(f"enemies inside function: {enemies}")
-    return enemies + 1
+# def increase_enemies():
+#     # global enemies
+#     # to make variable accessible in a function
+#     # enemies += 1
+#     print(f"enemies inside function: {enemies}")
+#     return enemies + 1
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+# Global Constants
+# Use upper case to realise that not to change variables
+PI = 3.14159
