12bd26e87db88ea0d0a6501478456990d48fcd27
thitiphatk <94935300+thitiphatko@users.noreply.github.com>
2022-01-28 16:38:35 +0000

    update day-12: modifying global scope

--- day-12/day-12-start.py
+++ day-12/day-12-start.py
@@ -32,10 +32,24 @@
 
 # There is no Block Scope
 
-game_level = 3
-def create_enemy():
-    enemies = ["Skeleton", "Zombie", "Alien"]
-    if game_level < 5:
-        new_enemy = enemies[0]
+# game_level = 3
+# def create_enemy():
+#     enemies = ["Skeleton", "Zombie", "Alien"]
+#     if game_level < 5:
+#         new_enemy = enemies[0]
 
-    print(new_enemy)
+#     print(new_enemy)
+
+# Modifying Global Scope
+
+enemies = 1
+
+def increase_enemies():
+    # global enemies
+    # to make variable accessible in a function
+    # enemies += 1
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
